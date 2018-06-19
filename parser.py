74002fde46694f44c91d2cca54165cdb4372982f
Parallels <nrbontha@gmail.com>
2018-06-19 10:51:01 -0500

    Adds docstrings

--- parser.py
+++ parser.py
@@ -2,8 +2,19 @@
 
 import pandas as pd
 
-def parse_row(row):
+def parse_attr(row):
+    """
+    Parse attributes row.
 
+    Parameters
+    ----------
+    row : str
+        Attribute value
+    
+    >>> row = "Cup:F|Band:28|Color:BLACK"
+    {'Cup': 'F', 'Band': 28, 'Color': 'BLACK'}
+
+    """
     parsed = {}
     
     for string in row.split("|"):
@@ -12,12 +23,29 @@ def parse_row(row):
     return parsed
 
 def parse_df(df, col, id):
+    """
+    Format pd.DataFrame with parsed attributes in 
+    separate columns.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Raw data with attributes col
+    col : str
+        Attributes col name
+    id : str
+        Unique ID in df
     
+    Returns
+    ----------
+    pd.DataFrame
+
+    """
     attrs = []
 
     for index, row in df.iterrows():
         if pd.notna(row[col]):
-            row_attrs = parse_row(row[col])
+            row_attrs = parse_attr(row[col])
             row_attrs[id] = row[id]
             attrs.append(row_attrs)
            
