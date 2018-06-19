495d49604036855bd3b59736cf7bf8f0d95cbe5c
Parallels <nrbontha@gmail.com>
2018-06-19 10:35:36 -0500

    Breaks out test from main script

--- parser.py
+++ parser.py
@@ -23,13 +23,3 @@ def parse_df(df, col, id):
            
     return pd.merge(df, pd.DataFrame(attrs), how='left', on=id)
 
-if __name__ == "__main__":
-
-    df = pd.read_csv('data/items_raw.csv')
-    clean = df[['ITEM_SID', 'STYLE_SID', 'SET', 'ATTR']] 
-
-    export = parse_df(df, 'ATTR', 'ITEM_SID')
-    export.to_csv('data/export.csv', index=False)
-
-    export_clean = parse_df(clean, 'ATTR', 'ITEM_SID')
-    export_clean.to_csv('data/export_clean.csv', index=False)
